e8eedb8e1f258d2005184f085bb40ff9067bb36e
offshoreCerag <57848264+offshoreCerag@users.noreply.github.com>
2020-08-03 21:20:02 +0530

    Swaggr ahd H@ db added

--- src/main/java/com/project/json2sql/controller/MainController.java
+++ src/main/java/com/project/json2sql/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,15 @@ import com.project.json2sql.dto.MainJson;
 import com.project.json2sql.model.OwnerDetails;
 import com.project.json2sql.service.ProcessService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+
+@RequestMapping("/json2sql")
+@Api(value="json2sql", description="Operations pertaining to JSON in SQL")
 public class MainController {
 	
 	public static final Logger logger = LoggerFactory.getLogger(MainController.class);
@@ -24,7 +33,16 @@ public class MainController {
 	@Autowired
 	ProcessService processService;
 
-	@PostMapping("/getProcess")
+
+	@ApiOperation(value = "Store Properties from JSON to SQL")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Successfully Stored"),
+	        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
+	@PostMapping("/getJsonStoreSql")
 	@ResponseBody
 	public ResponseEntity<?> getProcess(@RequestBody MainJson jsonObj) {
 		logger.info("Process Start");
@@ -41,7 +59,15 @@ public class MainController {
 		}
 	}
 	
-	@PostMapping("/getProxyData")
+	@ApiOperation(value = "Store Proxy Data from JSON to SQL")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Successfully Stored"),
+	        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+	        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+	        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	}
+	)
+	@PostMapping("/getProxyDataStoreSql")
 	@ResponseBody
 	public ResponseEntity<?> getProxyData(@RequestBody InputProxyDto inputObj) {
 		logger.info("Process Start");
